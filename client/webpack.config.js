efdd2a891649799097fb79e7f93c88a513ac6ddf
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-02-28 11:49:00 -0800

    update webpack config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const { GenerateSW } = require('workbox-webpack-plugin');
 const path = require('path');
 
 module.exports = () => {
@@ -11,17 +14,60 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin'
+      }),
+      new GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true
+      }),
+      new WebpackPwaManifest({
+        filename: "manifest.json",
+        name: "App",
+        orientation: "portrait",
+        display: "standalone",
+        start_url: "./",
+        crossorigin: null,
+        inject: true,
+        fingerprints: true,
+        ios: false,
+        publicPath: "./",
+        includeDirectory: true,
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }]
+      }),
      
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+      ]
     }
   };
 };
